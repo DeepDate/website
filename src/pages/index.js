ac58afb9ab877ad1b19751f0d9782a83a867c854
kovarny <sivochkin@gmail.com>
2023-09-20 18:15:00 +1000

    title fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,6 +31,6 @@ const IndexPageThree = () => (
  *
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
-export const Head = () => <Seo title="Home 3" />;
+export const Head = () => <Seo title="Home" />;
 
 export default IndexPageThree;
